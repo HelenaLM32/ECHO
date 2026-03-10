67f89f2d58621e80152ab8cf2788fd580ee9bb59
Flamante <lorienbimc@gmail.com>
2026-03-10 19:28:58 +0100

    initial css and jsx

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <>
+    <div className="main">
+        <div className="left">
+
+            <div className="title">Crea una cuenta</div>
+
+            <input type="text" placeholder="Correo electronico"/>
+            <input type="text" placeholder="Nombre de usuario"/>
+            <input type="text" placeholder="Contraseña"/>
+
+            <div className="btn-register">Registrarse</div>
+            <div className="divider"></div>
+        </div>
+        <div className="right">
+            echo placeholder
+        </div>
+    </div>
+    </>
   )
 }
