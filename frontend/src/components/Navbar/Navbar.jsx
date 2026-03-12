fde298f81ce098d2229dd91e479a5a9289fec742
adk <izanarandav@gmail.com>
2026-03-12 19:29:26 +0100

    navbar-almost-complete

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./Navbar.css";
+import placeholder from '../../assets/ECHO_placeholder.png';
 
 export default function Navbar() {
+  
   return (
+    
     <div className="nav-bar">
-        <a href="/home">HOME</a>
-        <a href="/login">LOGIN</a>
-        <a href="/register">REGISTER</a>
+      <div className="left-nav">
+        <img src={placeholder} className="logo-echo" alt="echo" />
+        <a href="/" className="a-left">Explorar</a>
+        <a href="/" className="a-left">Trabajos</a>
+        <a href="/" className="a-left">Recursos</a>
+      </div>
+      <div className="right-nav">
+        <div className="button-container" id="btn-1">
+          <a href="/register" className="a-right" id="a-1">Registrarse</a>
+        </div>
+        <div className="button-container" id="btn-2">
+          <a href="/login" className="a-right" id="a-2">Iniciar sesión</a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
