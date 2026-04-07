2b777a13bad56e916bf62d0b4f0307316b08b614
adk <izanarandav@gmail.com>
2026-04-07 21:17:57 +0200

    eliminar profile para evitar conflictos

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,67 +1,9 @@
-import { useAuth } from "../../context/AuthContext";
-import "./Profile.css";
+import React from 'react'
 
-export default function Profile() {
-  const { user } = useAuth();
-
-  if (!user) return null;
-
+function Profile() {
   return (
-    <div className="profile-container">
-      <div className="profile-header-container">
-        <img src="" alt="" />
-      </div>
-      <div className="profile-main-container-nav"></div>
-
-      <div className="profile-main-container">
-        <div className="profile-main-container-left">
-          <div id="main-container-left-nameinfo">
-            <img src="" alt="" className="icon-profile" />
-            <p id="username">{user.username}</p>
-            <p id="location">Ubicacion</p>
-          </div>
-          <div id="main-container-left-buttons">
-            <div className="btn-profile" id="btn-secondary-profile">
-              Editar informacion del perfil
-            </div>
-            <div className="btn-profile" id="btn-primary-profile">
-              Personalizar perfil
-            </div>
-          </div>
-          <div id="main-container-left-workexperience">
-            3
-          </div>
-        </div>
-        <div className="profile-main-container-right">
-          <div id="main-container-right-section">
-            <div className="section-button-profile">Trabajos</div>
-            <div className="section-button-profile">Valoraciones</div>
-            <div className="section-button-profile">Productos</div>
-            <div className="section-button-profile">Servicios</div>
-            <div className="section-button-profile">Estadisticas</div>
-
-          </div>
-          <div id="main-container-right-proyects">
-            <h3 className="h3-profile">Proyectos</h3>
-            <div className="create-project-box"></div>
-          </div>
-          <div id="main-container-right-section">
-            <div className="second-section-button-profile">Calendario</div>
-            <div className="second-section-button-profile">Compras</div>
-          </div>
-          <div id="main-container-right-subsection">
-            4
-          </div>
-        </div>
-      </div>
-    </div>
-
-    // <div className="profile-container">
-    //   <h1>Perfil</h1>
-    //   <div className="profile-card">
-    //     <p><strong>Usuario:</strong> {user.username}</p>
-    //     <p><strong>Correo:</strong> {user.email}</p>
-    //   </div>
-    // </div>
-  );
+    <div>Profile</div>
+  )
 }
+
+export default Profile
