895dcc01d185ab73e59614498e40f3da9c5ac17c
Flamante <lorienbimc@gmail.com>
2026-05-14 16:05:13 +0200

    fixed uploads and admin panel

--- backend/src/main/java/com/example/echo/presentation/rest/AdminDevController.java
+++ backend/src/main/java/com/example/echo/presentation/rest/AdminDevController.java
@@ -1,10 +1,16 @@
 package com.example.echo.presentation.rest;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.echo.core.entity.ordermessages.appservices.OrderMessageService;
 import com.example.echo.core.entity.orders.appservices.OrderService;
@@ -14,7 +20,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
-@Profile("dev")
 @RequestMapping("/admin/dev")
 public class AdminDevController {
 
